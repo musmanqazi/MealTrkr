d1ab93cc539512c4d23edeb806b670e5a6343e15
Cope <apc42190@maclab045.fullerton.edu>
2022-12-07 17:03:12 -0800

    Meals table now works. Need to work on getting images in

--- MealTrkr/MealTrkr/MealsController.swift
+++ MealTrkr/MealTrkr/MealsController.swift
@@ -9,13 +9,17 @@ import UIKit
 import CoreData
 
 
-class MealsController: UITableViewController{
+class MealsController: UIViewController{
     
     let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    var saved_meals : [Meal] = []
     
+    @IBOutlet var tableView : UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -30,11 +34,34 @@ class MealsController: UITableViewController{
         context.performAndWait {
             fetchedMeals = try? fetchRequest.execute()
         }
-        print("Meals View loaded. CoreData founds \(fetchedMeals!.count) meals stored.")
+        saved_meals = fetchedMeals!
+        print("Meals View loaded. CoreData founds \(saved_meals.count) meals stored.")
+        tableView.reloadData()
     }
-
-    
-    
     
 }
 
+extension MealsController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Hello")
+    }
+}
+
+extension MealsController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return saved_meals.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy/MM/dd HH:mma"
+        
+        let meal = saved_meals[indexPath.row]
+        cell.textLabel?.text = dateFormatter.string(from: meal.date!)
+        cell.detailTextLabel?.text = "Calories: \(meal.calories)\nCarbs: \(meal.carbs)\nProtein: \(meal.protein)\nSaturated Fat: \(meal.saturated_fat)\nSodium: \(meal.sodium)\nCholesterol: \(meal.cholesterol)"
+        
+        return cell
+    }
+}
